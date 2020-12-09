ac77f4c109ba4c0f66a05e3d45f9b445c8eb1f69
zhum <zhumeng@skyvis.com.cn>
2020-12-09 21:29:18 +0800

    1.自定义线程池 2.Stream流式计算

--- src/main/java/cn/com/dmg/juc/MyThreadPoolDemo.java
+++ src/main/java/cn/com/dmg/juc/MyThreadPoolDemo.java
@@ -2,9 +2,7 @@ package cn.com.dmg.juc;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * 线程池
@@ -15,19 +13,24 @@ import java.util.concurrent.Executors;
 public class MyThreadPoolDemo {
 
     public static void main(String[] args) {
-        //List list = new ArrayList();
-        //List list = Arrays.asList("a","b");
-        //固定数的线程池，一池五线程
-
-       //ExecutorService threadPool =  Executors.newFixedThreadPool(5); //一个银行网点，5个受理业务的窗口
-       //ExecutorService threadPool =  Executors.newSingleThreadExecutor(); //一个银行网点，1个受理业务的窗口
-       ExecutorService threadPool =  Executors.newCachedThreadPool(); //一个银行网点，可扩展受理业务的窗口
-
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        ExecutorService threadPool = new ThreadPoolExecutor(
+                2,
+                5,
+                2L,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(3),
+                Executors.defaultThreadFactory(),
+                //new ThreadPoolExecutor.AbortPolicy()
+                //new ThreadPoolExecutor.CallerRunsPolicy()
+                //new ThreadPoolExecutor.DiscardOldestPolicy()
+                new ThreadPoolExecutor.DiscardOldestPolicy()
+        );
         //10个顾客请求
         try {
-            for (int i = 1; i <=10; i++) {
-                threadPool.execute(()->{
-                    System.out.println(Thread.currentThread().getName()+"\t 办理业务");
+            for (int i = 1; i <= 10; i++) {
+                threadPool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "\t 办理业务");
                 });
             }
         } catch (Exception e) {
@@ -36,7 +39,32 @@ public class MyThreadPoolDemo {
             threadPool.shutdown();
         }
 
+
     }
+
+    private static void threadPool() {
+        //List list = new ArrayList();
+        //List list = Arrays.asList("a","b");
+        //固定数的线程池，一池五线程
+
+//       ExecutorService threadPool =  Executors.newFixedThreadPool(5); //一个银行网点，5个受理业务的窗口
+//       ExecutorService threadPool =  Executors.newSingleThreadExecutor(); //一个银行网点，1个受理业务的窗口
+        ExecutorService threadPool = Executors.newCachedThreadPool(); //一个银行网点，可扩展受理业务的窗口
+
+        //10个顾客请求
+        try {
+            for (int i = 1; i <= 10; i++) {
+                threadPool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "\t 办理业务");
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+
 }
  
  
